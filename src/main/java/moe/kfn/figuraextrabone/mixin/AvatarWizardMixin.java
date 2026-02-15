ef47b4ff4d7f55daf748875d573eec4849992380
Kafune CH <kafune.ch@gmail.com>
2026-02-15 11:48:25 +0700

    reverse: old format script

--- src/main/java/moe/kfn/figuraextrabone/mixin/AvatarWizardMixin.java
+++ src/main/java/moe/kfn/figuraextrabone/mixin/AvatarWizardMixin.java
@@ -120,10 +120,10 @@ public class AvatarWizardMixin {
                         { models.model_blend.root.Body.chest, "body" },
                         { models.model_blend.root.LeftShoulder, "body" },
                         { models.model_blend.root.RightShoulder, "body" },
-                        { models.model_blend.root.LeftShoulder.LeftArm.LArmLower, "left_arm" },
-                        { models.model_blend.root.RightShoulder.RightArm.RArmLower, "right_arm" },
-                        { models.model_blend.root.LeftLeg.LLegLower, "left_leg" },
-                        { models.model_blend.root.RightLeg.RLegLower, "right_leg" }
+                        { models.model_blend.root.LeftShoulder.LeftArm.LArmLower, "leftArm" },
+                        { models.model_blend.root.RightShoulder.RightArm.RArmLower, "rightArm" },
+                        { models.model_blend.root.LeftLeg.LLegLower, "leftLeg" },
+                        { models.model_blend.root.RightLeg.RLegLower, "rightLeg" }
                     })
                     """;
 
